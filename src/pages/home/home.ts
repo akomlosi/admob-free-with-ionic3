0690fef11586c14dacae0705a46445e8b2a59e5b
ebolatumor <komlosi.attila@gmail.com>
2018-03-24 09:26:34 +0100

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig} from '@ionic-native/admob-free';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, public admob: AdMobFree) {
 
-  }
+	}
+
+	showBanner() {
+
+		let bannerConfig: AdMobFreeBannerConfig = {
+			isTesting: true, // Remove in production
+			autoShow: true,
+			id: 'ca-app-pub-5742208320359495/3836512351'
+		};
+
+		this.admob.banner.config(bannerConfig);
+
+		this.admob.banner.prepare().then(() => {
+			// success
+		}).catch(e => console.log(e));
+
+	}
 
 }
